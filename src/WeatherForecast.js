1bcd5f7d99c80426b89eb2ac0f1566724e24b7b5
Aleksandr Honchar <olddickinson@gmail.com>
2022-12-26 08:25:45 +0200

    remove file

--- src/WeatherForecast.js
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from "react";
-import "./WeatherForecast.css";
-import axios from "axios";
-import WeatherForecastDay from "./WeatherForecastDay";
-
-export default function WeatherForecast(props) {
-
-}
