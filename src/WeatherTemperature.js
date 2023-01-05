0fc6236da7e1f5e6b80560d521fb961bb1b8076b
HonH91 <113532577+HonH91@users.noreply.github.com>
2023-01-05 08:18:34 -0700

    Removed conversion links

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,44 +1,15 @@
 import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-    const [unit, setUnit] = useState("celsius");
-    function fahrenheitShow(event) {
-        event.preventDefault();
-        setUnit("fahrenheit");
-    }
-
-    function celsiusShow(event) {
-        event.preventDefault();
-        setUnit("celsius");
-    }
-
-    if (unit === "celsius") {
-        return (
-            <div className="WeatherTemperature">
-                <span className="temperature">{Math.round(props.celsius)}</span>
-                <span className="unit">
-                    °C  |{" "}
-                    <a href="/" onClick={fahrenheitShow}>
-                        °F
-                    </a>
-                </span>
-            </div>
-        )
-    }
-    else {
-        return (
+     return (
             <div className="WeatherTemperature">
                 <span className="temperature">
                     {Math.round(props.celsius)}
                 </span>
                 <span className="unit">
                     <a href="/" onClick={celsiusShow}>
-                        °C  |{" "}
-                    </a>
-                    °F
+                        °C  
                 </span>
             </div>
         )
-    }
-
 }
